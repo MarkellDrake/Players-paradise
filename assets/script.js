72c601f18ad35fbb2565371400311960315aae17
Josh Sandquist <joshdsand@gmail.com>
2023-03-08 21:36:39 -0700

    added Youtibe API fetch requests

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 var RAWGAPIKey = '227c60318fed4aafabdb435450204c35'
 var URL = 'https://api.rawg.io/api/games?search='
-var searchbuttonEL = document.querySelector('.button is-primary');
-var searchBarEl = document.querySelector('.input search-bar');
+var searchbuttonEL = document.querySelector('.button .is-primary');
+var searchBarEl = document.querySelector('.input .search-bar');
 var yearEl = document.querySelector('#year');
 var metacriticScoreEl = document.querySelector('#metacriticScore');
 var esrbRatingEl = document.querySelector('#esrbRating');
@@ -22,7 +22,12 @@ document.searchBarEl.value = ""
 searchbuttonEL.addEventListener('click',searchGame);
 
 
-
+// KEEP THIS LINK FOR GATHERING DESCRIPTION
+//https://api.rawg.io/api/games/23165?&key=227c60318fed4aafabdb435450204c35
+//This will be used as a second fetch request to gather the data by game Id
+//The first fetch request will be used only to grab the game ID
+//The game ID will be added to the second URL
+//The second fetch request will be used to gather all other data for the game
 
 var testFunction = function () {
 var testURL = 'https://api.rawg.io/api/games?search=mario%20kart%8&search_exact=true&metacritic=1,100&ordering=-released&key=227c60318fed4aafabdb435450204c35'
@@ -50,3 +55,31 @@ fetch(testURL)
 }
 
 'https://api.rawg.io/api/games?search='+ 'mario%20kart' + '&search_exact=true&metacritic=1,100&ordering=-released&key=227c60318fed4aafabdb435450204c35'
+
+//function to the YouTube API
+var youtubeTest = function () {
+    //Will need to add user input into the link
+    //Link searches key words provided by the user
+    var searchUrl = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyBZdZHH3hqAOG4qxy3IiD3zQgHcun_aIG4&part=snippet&type=video&maxResults=1&q=mario%20kart%20review';
+//First fetch is used to grab the videoID data from the API for further use.
+    fetch(searchUrl)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        var videoId = data.items[0].id.videoId;
+        //Creating a new URL using the videoID data that can be used for embedding the video
+        var videosUrl = 'https://www.googleapis.com/youtube/v3/videos?key=AIzaSyBZdZHH3hqAOG4qxy3IiD3zQgHcun_aIG4&part=player,snippet&id=' + videoId;
+        fetch(videosUrl)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data) {
+            //Acessing the HTML embed link for the chosen video
+            var videoUrl = data.items[0].player.embedHtml;
+            //Uses the replace method to create a youtube video embed code to be inserted into the HTML.
+            var videoEmbedCode = videoUrl.replace(/.*\/embed\/(.*)".*/, '<iframe width="560" height="315" src="//www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>');
+            document.querySelector('#videoContainer').innerHTML = videoEmbedCode;
+        })
+    })
+}
